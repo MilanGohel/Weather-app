9eb33e17393a93a8a0906ddf09cf707115a90a57
MilanGohel <milan.gohel66@gmail.com>
2024-02-17 12:00:49 +0530

    initial commit

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Key": "401986bfa8msh3d4a5795184b9aep183fc2jsn06cf4360479f",// enter your rapid api key here
+      "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+    },
+  };
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+  
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "4de442b569062270f14b292269d6bb4e"; // enter your key from openweather API
---
+++ src/components/search/Search.jsx
@@ -0,0 +1,51 @@
+import React, { useState } from "react";
+import { AsyncPaginate } from "react-select-async-paginate";
+import { geoApiOptions, GEO_API_URL } from "../../api";
+
+const Search = ({ onSearchChange }) => {
+  const [search, setSearch] = useState(null);
+
+  const loadOptions = (inputValue) => {
+    return fetch(
+      `${GEO_API_URL}/cities?minPopulation=1000000&namePrefix=${inputValue}`,
+      geoApiOptions
+    )
+      .then((response) => response.json())
+      .then((response) => {
+        return {
+          options: response.data.map((city) => {
+            return {
+              value: `${city.latitude} ${city.longitude}`,
+              label: `${city.name}, ${city.countryCode}`,
+            };
+          }),
+        };
+      });
+  };
+
+  const handleOnChange = (searchData) => {
+    console.log(searchData)
+    setSearch(searchData);
+    onSearchChange(searchData);
+  };
+  const myStyles = {
+    zIndex: 10,
+    postion: "relative",
+  }
+  return (
+    <div style={{zIndex: 100, position: "relative",color:"black"}}>
+    <AsyncPaginate
+      placeholder="Search for city"
+      debounceTimeout={600}
+      value={search}
+      onChange={handleOnChange}
+      loadOptions={loadOptions}
+    />
+    </div>
+    // <>
+    // <input placeholder="Search for city" />
+    // </>
+  );
+};
+
+export default Search;
